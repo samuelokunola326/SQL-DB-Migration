9f41a0c4e6b8f73a889a6f2df23501916395ef1f
samuelokunola326 <samuelokunola@gmail.com>
2020-10-15 21:45:18 -0400

    updates to table schema

--- table_queries.sql
+++ table_queries.sql
@@ -5,3 +5,40 @@ CREATE TABLE titles(
 	title VARCHAR(50)
 
 );
+
+DROP TABLE employees;
+
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL PRIMARY KEY,
+	title_id VARCHAR(30) NOT NULL,
+	birth_date DATE,
+	first_Name VARCHAR(100),
+	last_Name VARCHAR(100),
+	sex VARCHAR(10),
+	hire_date DATE,
+	FOREIGN KEY(title_id)
+		REFERENCES titles(title_id)
+	
+);
+
+
+DROP TABLE salaries;
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary MONEY,
+	FOREIGN KEY(emp_no)
+		REFERENCES employees(emp_no)
+
+);
+
+
+DROP TABLE depts;
+
+
+CREATE TABLE depts(
+	dept_no VARCHAR(30)NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30)
+
+);
